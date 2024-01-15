346a0878047a2536fdb505aae88964205008f59e
Supanut Onsri <105142244+supanut1998@users.noreply.github.com>
2024-01-15 14:24:44 +0700

    Update test-spark.py

--- dags/test-spark.py
+++ dags/test-spark.py
@@ -20,7 +20,7 @@ dag = DAG(
 # Define the SparkSubmitOperator task
 submit_spark_job = SparkSubmitOperator(
     task_id='submit_spark_job',
-    application='test-spark.py',  # Path to your Spark application
+    application='spark-file.py',  # Path to your Spark application
     conn_id='spark_default',  # Connection ID for Spark
     executor_cores=2,
     executor_memory='2g',
